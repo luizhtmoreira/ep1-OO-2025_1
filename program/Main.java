0be65c60ac9ba4b335569d4e9a74c9877f4715e2
luizhtmoreira <lht.unb@gmail.com>
2025-05-19 00:13:09 -0300

    Main com menu inicial e menu do modo Aluno

---
+++ program/Main.java
@@ -0,0 +1,85 @@
+package program;
+
+import entities.*;
+import java.util.*;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        carregarDados();
+        
+        int opcao;
+        do {
+            System.out.println("\n=== SISTEMA ACADÊMICO FCTE ===");
+            System.out.println("1. Modo Aluno");
+            System.out.println("2. Modo Disciplina/Turma");
+            System.out.println("3. Modo Avaliação/Frequência");
+            System.out.println("4. Salvar e Sair");
+            System.out.print("Escolha uma opção: ");
+            
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch (opcao) {
+                case 1:
+                    modoAluno();
+                    break;
+                case 2:
+                    modoDisciplinaTurma();
+                    break;
+                case 3:
+                    modoAvaliacaoFrequencia();
+                    break;
+                case 4:
+                    salvarDados();
+                    System.out.println("Dados salvos. Saindo do sistema...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (opcao != 4);
+    }
+    
+    private static void modoAluno() {
+        int opcao;
+        do {
+            System.out.println("\n=== MODO ALUNO ===");
+            System.out.println("1. Cadastrar Aluno");
+            System.out.println("2. Editar Aluno");
+            System.out.println("3. Listar Alunos");
+            System.out.println("4. Matricular em Disciplina");
+            System.out.println("5. Trancar Disciplina");
+            System.out.println("6. Voltar ao Menu Principal");
+            System.out.print("Escolha uma opção: ");
+            
+            opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpar buffer
+            
+            switch (opcao) {
+                case 1:
+                    cadastrarAluno();
+                    break;
+                case 2:
+                    editarAluno();
+                    break;
+                case 3:
+                    listarAlunos();
+                    break;
+                case 4:
+                    matricularAluno();
+                    break;
+                case 5:
+                    trancarDisciplina();
+                    break;
+                case 6:
+                    System.out.println("Retornando ao menu principal...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (opcao != 6);
+    }
+    
+}
